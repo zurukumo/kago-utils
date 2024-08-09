806284ad0ec2682e76cd2c293e2308df526ab320
zurukumo <zurukumo@gmail.com>
2024-08-09 13:36:13 +0900

    feat: create hai.py and test for hai.py

---
+++ src/hai.py
@@ -0,0 +1,266 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+T1 = TypeVar('T1', bound='Hai34')
+T2 = TypeVar('T2', bound='Hai136')
+
+
+class Hai(ABC):
+    def __init__(self, data):
+        self.data = self.normalize(data)
+
+    @abstractmethod
+    def normalize(self, data):
+        pass
+
+    @abstractmethod
+    def __add__(self, other):
+        pass
+
+    @abstractmethod
+    def __sub__(self, other):
+        pass
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] {self.data}"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Hai34(Hai, Generic[T1]):
+    @abstractmethod
+    def to_hai34_counter(self) -> 'Hai34Counter':
+        pass
+
+    @abstractmethod
+    def to_hai34_list(self) -> 'Hai34List':
+        pass
+
+    @abstractmethod
+    def to_hai34_string(self) -> 'Hai34String':
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def from_hai34_counter() -> T1:
+        pass
+
+    def __add__(self, other) -> T1:
+        if isinstance(other, (Hai34, Hai136)):
+            new_data = [a + b for a, b in zip(self.to_hai34_counter().data, other.to_hai34_counter().data)]
+            return self.__class__.from_hai34_counter(Hai34Counter(new_data))
+
+        raise TypeError(f"unsupported operand type(s) for +: '{type(self).__name__}' and '{type(other).__name__}'")
+
+    def __sub__(self, other) -> T1:
+        if isinstance(other, (Hai34, Hai136)):
+            new_data = [a - b for a, b in zip(self.to_hai34_counter().data, other.to_hai34_counter().data)]
+            return self.__class__.from_hai34_counter(Hai34Counter(new_data))
+
+        raise TypeError(f"unsupported operand type(s) for -: '{type(self).__name__}' and '{type(other).__name__}'")
+
+
+class Hai136(Hai, Generic[T2]):
+    @abstractmethod
+    def to_hai34_counter(self) -> 'Hai34Counter':
+        pass
+
+    @abstractmethod
+    def to_hai34_list(self) -> 'Hai34List':
+        pass
+
+    @abstractmethod
+    def to_hai34_string(self) -> 'Hai34String':
+        pass
+
+    @abstractmethod
+    def to_hai136_list(self) -> 'Hai136List':
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def from_hai136_list() -> T2:
+        pass
+
+    def __add__(self, other) -> T2:
+        if isinstance(other, Hai136):
+            new_data = self.to_hai136_list().data + other.to_hai136_list().data
+            return self.__class__.from_hai136_list(Hai136List(new_data))
+
+        raise TypeError(f"unsupported operand type(s) for +: '{type(self).__name__}' and '{type(other).__name__}'")
+
+    def __sub__(self, other) -> T2:
+        if isinstance(other, Hai136):
+            new_data = self.to_hai136_list().data.copy()
+            for hai in other.to_hai136_list().data:
+                new_data.remove(hai)
+            return self.__class__.from_hai136_list(Hai136List(new_data))
+
+        raise TypeError(f"unsupported operand type(s) for -: '{type(self).__name__}' and '{type(other).__name__}'")
+
+
+class Hai34Counter(Hai34['Hai34Counter']):
+    def normalize(self, data):
+        if len(data) != 34:
+            raise ValueError(f"invalid data: len(data) = {len(data)} (expected 34)")
+        for k, v in enumerate(data):
+            if not isinstance(v, int):
+                raise ValueError(f"invalid data: type(data[{k}]) = {type(v)} (expected int)")
+            if not 0 <= v <= 4:
+                raise ValueError(f"invalid data: data[{k}] = {v} (expected 0 <= v <= 4)")
+
+        return data
+
+    def to_hai34_counter(self):
+        return self
+
+    def to_hai34_list(self):
+        hai_list = []
+        for hai, count in enumerate(self.data):
+            for _ in range(count):
+                hai_list.append(hai)
+        return Hai34List(hai_list)
+
+    def to_hai34_string(self):
+        items = {'m': [], 'p': [], 's': [], 'z': []}
+        for hai, count in enumerate(self.data):
+            for _ in range(count):
+                if hai < 9:
+                    items['m'] .append(hai + 1)
+                elif hai < 18:
+                    items['p'].append(hai - 8)
+                elif hai < 27:
+                    items['s'].append(hai - 17)
+                else:
+                    items['z'].append(hai - 26)
+
+        hai_string = ''
+        for suit in items:
+            if items[suit]:
+                hai_string += f"{''.join(map(str, items[suit]))}{suit}"
+        return Hai34String(hai_string)
+
+    @staticmethod
+    def from_hai34_counter(hai34_counter: 'Hai34Counter'):
+        return hai34_counter
+
+
+class Hai34List(Hai34['Hai34List']):
+    def normalize(self, data):
+        for hai in data:
+            if not isinstance(hai, int):
+                raise ValueError(f"invalid data: type({hai}) = {type(hai)} (expected int)")
+            if not 0 <= hai <= 33:
+                raise ValueError(f"invalid data: {hai} (expected 0 <= value <= 33)")
+
+        for i in range(34):
+            if not 0 <= data.count(i) <= 4:
+                raise ValueError(f"invalid data: count({i}) = {data.count(i)} (expected 0 <= count <= 4)")
+        return sorted(data)
+
+    def to_hai34_counter(self):
+        hai_counter = [0] * 34
+        for hai in self.data:
+            hai_counter[hai] += 1
+        return Hai34Counter(hai_counter)
+
+    def to_hai34_list(self):
+        return self
+
+    def to_hai34_string(self):
+        return self.to_hai34_counter().to_hai34_string()
+
+    @ staticmethod
+    def from_hai34_counter(hai34_counter: 'Hai34Counter'):
+        return hai34_counter.to_hai34_list()
+
+
+class Hai34String(Hai34['Hai34String']):
+    def normalize(self, data):
+        items = {'m': [0] * 9, 'p': [0] * 9, 's': [0] * 9, 'z': [0] * 9}
+        suit = ''
+        for c in reversed(data):
+            if c in '123456789':
+                if suit == '':
+                    raise ValueError("invalid data: there is a number without suit")
+                items[suit][int(c) - 1] += 1
+            elif c in 'mpsz':
+                suit = c
+            else:
+                raise ValueError(f"invalid data: {c} (expected 'mpsz' or '123456789')")
+
+        for suit in items:
+            for k, v in enumerate(items[suit]):
+                if not 0 <= v <= 4:
+                    raise ValueError(f"invalid data: count({suit}{k + 1}) = {v} (expected 0 <= count <= 4)")
+
+        data = ''
+        for suit in items:
+            for k, v in enumerate(items[suit]):
+                data += f"{k + 1}" * v
+            if sum(items[suit]) > 0:
+                data += suit
+
+        return data
+
+    def to_hai34_counter(self):
+        hai_counter = [0] * 34
+        suit = ''
+        for c in reversed(self.data):
+            if c in '123456789':
+                hai = int(c) - 1
+                if suit == 'm':
+                    hai_counter[hai] += 1
+                elif suit == 'p':
+                    hai_counter[hai + 9] += 1
+                elif suit == 's':
+                    hai_counter[hai + 18] += 1
+                elif suit == 'z':
+                    hai_counter[hai + 27] += 1
+            elif c in 'mpsz':
+                suit = c
+        return Hai34Counter(hai_counter)
+
+    def to_hai34_list(self):
+        return self.to_hai34_counter().to_hai34_list()
+
+    def to_hai34_string(self):
+        return self
+
+    @ staticmethod
+    def from_hai34_counter(hai34_counter: 'Hai34Counter'):
+        return hai34_counter.to_hai34_string()
+
+
+class Hai136List(Hai136['Hai136List']):
+    def normalize(self, data):
+        for hai in data:
+            if not isinstance(hai, int):
+                raise ValueError(f"invalid data: type({hai}) = {type(hai)} (expected int)")
+            if not 0 <= hai <= 135:
+                raise ValueError(f"invalid data: {hai} (expected 0 <= value <= 135)")
+
+        for i in range(136):
+            if not 0 <= data.count(i) <= 1:
+                raise ValueError(f"invalid data: count({i}) = {data.count(i)} (expected 0 <= count <= 1)")
+        return sorted(data)
+
+    def to_hai34_counter(self):
+        hai_counter = [0] * 34
+        for hai in self.data:
+            hai_counter[hai // 4] += 1
+        return Hai34Counter(hai_counter)
+
+    def to_hai34_list(self):
+        return self.to_hai34_counter().to_hai34_list()
+
+    def to_hai34_string(self):
+        return self.to_hai34_counter().to_hai34_string()
+
+    def to_hai136_list(self):
+        return self
+
+    @ staticmethod
+    def from_hai136_list(hai136_list: 'Hai136List'):
+        return hai136_list
---
+++ tests/test_hai.py
@@ -0,0 +1,431 @@
+import unittest
+
+from src.hai import Hai34Counter, Hai34List, Hai34String, Hai136List
+
+
+class TestInit(unittest.TestCase):
+    def test_hai34_counter_valid(self):
+        data = [1] + [0] * 33
+        Hai34Counter(data)
+
+    def test_hai34_counter_too_short_length(self):
+        data = [1]
+        with self.assertRaises(ValueError):
+            Hai34Counter(data)
+
+    def test_hai34_counter_too_long_length(self):
+        data = [1] + [0] * 34
+        with self.assertRaises(ValueError):
+            Hai34Counter(data)
+
+    def test_hai34_counter_too_large_value(self):
+        data = [5] + [0] * 33
+        with self.assertRaises(ValueError):
+            Hai34Counter(data)
+
+    def test_hai34_counter_too_small_value(self):
+        data = [-1] + [0] * 33
+        with self.assertRaises(ValueError):
+            Hai34Counter(data)
+
+    def test_hai34_counter_float_value(self):
+        data = [0.5] + [0] * 33
+        with self.assertRaises(ValueError):
+            Hai34Counter(data)
+
+    def test_hai34_list_valid(self):
+        data = [0]
+        Hai34List(data)
+
+    def test_hai34_list_too_small_value(self):
+        data = [-1]
+        with self.assertRaises(ValueError):
+            Hai34List(data)
+
+    def test_hai34_list_too_large_value(self):
+        data = [34]
+        with self.assertRaises(ValueError):
+            Hai34List(data)
+
+    def test_hai34_list_float_value(self):
+        data = [0.5]
+        with self.assertRaises(ValueError):
+            Hai34List(data)
+
+    def test_hai34_list_too_large_count(self):
+        data = [0] * 5
+        with self.assertRaises(ValueError):
+            Hai34List(data)
+
+    def test_hai34_string_valid(self):
+        data = '123m'
+        Hai34String(data)
+
+    def test_hai34_string_invalid_format(self):
+        data = '123m123'
+        with self.assertRaises(ValueError):
+            Hai34String(data)
+
+    def test_hai34_string_invalid_character(self):
+        data = '123x'
+        with self.assertRaises(ValueError):
+            Hai34String(data)
+
+    def test_hai34_string_too_large_count(self):
+        data = '11111m'
+        with self.assertRaises(ValueError):
+            Hai34String(data)
+
+    def test_hai136_list_valid(self):
+        data = [0]
+        Hai136List(data)
+
+    def test_hai136_list_too_small_value(self):
+        data = [-1]
+        with self.assertRaises(ValueError):
+            Hai136List(data)
+
+    def test_hai136_list_too_large_value(self):
+        data = [136]
+        with self.assertRaises(ValueError):
+            Hai136List(data)
+
+    def test_hai136_list_float_value(self):
+        data = [0.5]
+        with self.assertRaises(ValueError):
+            Hai136List(data)
+
+    def test_hai136_list_too_large_count(self):
+        data = [0] * 2
+        with self.assertRaises(ValueError):
+            Hai136List(data)
+
+
+class TestConvert(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.hai34_counter_data = [
+            1, 1, 1, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 1, 1, 1,
+            2, 0, 0, 0, 0, 0, 0
+        ]
+        cls.hai34_list_data = [0, 1, 2, 12, 13, 14, 24, 25, 26, 27, 27]
+        cls.hai136_list_data = [0, 4, 8, 48, 52, 56, 96, 100, 104, 108, 109]
+        cls.hai34_string_data = '123m456p789s11z'
+
+    def test_hai34_counter_to_hai34_counter(self):
+        result = Hai34Counter(self.hai34_counter_data).to_hai34_counter().data
+        expected = self.hai34_counter_data
+        self.assertEqual(result, expected)
+
+    def test_hai34_counter_to_hai34_list(self):
+        result = Hai34Counter(self.hai34_counter_data).to_hai34_list().data
+        expected = self.hai34_list_data
+        self.assertEqual(result, expected)
+
+    def test_hai34_counter_to_hai34_string(self):
+        result = Hai34Counter(self.hai34_counter_data).to_hai34_string().data
+        expected = self.hai34_string_data
+        self.assertEqual(result, expected)
+
+    def test_hai34_list_to_hai34_counter(self):
+        result = Hai34List(self.hai34_list_data).to_hai34_counter().data
+        expected = self.hai34_counter_data
+        self.assertEqual(result, expected)
+
+    def test_hai34_list_to_hai34_list(self):
+        result = Hai34List(self.hai34_list_data).to_hai34_list().data
+        expected = self.hai34_list_data
+        self.assertEqual(result, expected)
+
+    def test_hai34_list_to_hai34_string(self):
+        result = Hai34List(self.hai34_list_data).to_hai34_string().data
+        expected = self.hai34_string_data
+        self.assertEqual(result, expected)
+
+    def test_hai34_string_to_hai34_counter(self):
+        result = Hai34String(self.hai34_string_data).to_hai34_counter().data
+        expected = self.hai34_counter_data
+        self.assertEqual(result, expected)
+
+    def test_hai34_string_to_hai34_list(self):
+        result = Hai34String(self.hai34_string_data).to_hai34_list().data
+        expected = self.hai34_list_data
+        self.assertEqual(result, expected)
+
+    def test_hai34_string_to_hai34_string(self):
+        result = Hai34String(self.hai34_string_data).to_hai34_string().data
+        expected = self.hai34_string_data
+        self.assertEqual(result, expected)
+
+    def test_hai136_list_to_hai34_counter(self):
+        result = Hai136List(self.hai136_list_data).to_hai34_counter().data
+        expected = self.hai34_counter_data
+        self.assertEqual(result, expected)
+
+    def test_hai136_list_to_hai34_list(self):
+        result = Hai136List(self.hai136_list_data).to_hai34_list().data
+        expected = self.hai34_list_data
+        self.assertEqual(result, expected)
+
+    def test_hai136_list_to_hai136_list(self):
+        result = Hai136List(self.hai136_list_data).to_hai136_list().data
+        expected = self.hai136_list_data
+        self.assertEqual(result, expected)
+
+    def test_hai136_list_to_hai34_string(self):
+        result = Hai136List(self.hai136_list_data).to_hai34_string().data
+        expected = self.hai34_string_data
+        self.assertEqual(result, expected)
+
+
+class TestAdd(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.hai34_string1 = Hai34String('123m456p789s11z')
+        cls.hai34_counter1 = cls.hai34_string1.to_hai34_counter()
+        cls.hai34_list1 = cls.hai34_string1.to_hai34_list()
+        cls.hai136_list1 = Hai136List([0, 4, 8, 48, 52, 56, 96, 100, 104, 108, 109])
+
+        cls.hai34_string2 = Hai34String('23m456p78s1z')
+        cls.hai34_counter2 = cls.hai34_string2.to_hai34_counter()
+        cls.hai34_list2 = cls.hai34_string2.to_hai34_list()
+        cls.hai136_list2 = Hai136List([5, 9, 49, 53, 57, 97, 101, 110])
+
+        cls.hai34_string_sum = Hai34String('12233m445566p77889s111z')
+        cls.hai34_counter_sum = cls.hai34_string_sum.to_hai34_counter()
+        cls.hai34_list_sum = cls.hai34_string_sum.to_hai34_list()
+        cls.hai136_list_sum = Hai136List([0, 4, 5, 8, 9, 48, 49, 52, 53, 56, 57, 96, 97, 100, 101, 104, 108, 109, 110])
+
+    def test_add_hai34_counter_and_hai34_counter(self):
+        result = self.hai34_counter1 + self.hai34_counter2
+        expected = self.hai34_counter_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai34_counter_and_hai34_list(self):
+        result = self.hai34_counter1 + self.hai34_list2
+        expected = self.hai34_counter_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai34_counter_and_hai34_string(self):
+        result = self.hai34_counter1 + self.hai34_string2
+        expected = self.hai34_counter_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai34_counter_and_hai136_list(self):
+        result = self.hai34_counter1 + self.hai136_list2
+        expected = self.hai34_counter_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai34_counter_and_int(self):
+        with self.assertRaises(TypeError):
+            self.hai34_counter1 + 1
+
+    def test_add_hai34_list_and_hai34_counter(self):
+        result = self.hai34_list1 + self.hai34_counter2
+        expected = self.hai34_list_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai34_list_and_hai34_list(self):
+        result = self.hai34_list1 + self.hai34_list2
+        expected = self.hai34_list_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai34_list_and_hai34_string(self):
+        result = self.hai34_list1 + self.hai34_string2
+        expected = self.hai34_list_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai34_list_and_hai136_list(self):
+        result = self.hai34_list1 + self.hai136_list2
+        expected = self.hai34_list_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai34_list_and_int(self):
+        with self.assertRaises(TypeError):
+            self.hai34_list1 + 1
+
+    def test_add_hai34_string_and_hai34_counter(self):
+        result = self.hai34_string1 + self.hai34_counter2
+        expected = self.hai34_string_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai34_string_and_hai34_list(self):
+        result = self.hai34_string1 + self.hai34_list2
+        expected = self.hai34_string_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai34_string_and_hai34_string(self):
+        result = self.hai34_string1 + self.hai34_string2
+        expected = self.hai34_string_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai34_string_and_hai136_list(self):
+        result = self.hai34_string1 + self.hai136_list2
+        expected = self.hai34_string_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai34_string_and_int(self):
+        with self.assertRaises(TypeError):
+            self.hai34_string1 + 1
+
+    def test_add_hai136_list_and_hai34_counter(self):
+        with self.assertRaises(TypeError):
+            self.hai136_list1 + self.hai34_counter2
+
+    def test_add_hai136_list_and_hai34_list(self):
+        with self.assertRaises(TypeError):
+            self.hai136_list1 + self.hai34_list2
+
+    def test_add_hai136_list_and_hai34_string(self):
+        with self.assertRaises(TypeError):
+            self.hai136_list1 + self.hai34_string2
+
+    def test_add_hai136_list_and_hai136_list(self):
+        result = self.hai136_list1 + self.hai136_list2
+        expected = self.hai136_list_sum
+        self.assertEqual(result.data, expected.data)
+
+    def test_add_hai136_list_and_int(self):
+        hai136_list = self.hai136_list1
+        with self.assertRaises(TypeError):
+            hai136_list + 1
+
+
+class TestSub(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.hai34_string1 = Hai34String('123m456p789s11z')
+        cls.hai34_counter1 = cls.hai34_string1.to_hai34_counter()
+        cls.hai34_list1 = cls.hai34_string1.to_hai34_list()
+        cls.hai136_list1 = Hai136List([0, 4, 8, 48, 52, 56, 96, 100, 104, 108, 109])
+
+        cls.hai34_string2 = Hai34String('23m456p78s1z')
+        cls.hai34_counter2 = cls.hai34_string2.to_hai34_counter()
+        cls.hai34_list2 = cls.hai34_string2.to_hai34_list()
+        cls.hai136_list2 = Hai136List([4, 8, 48, 52, 56, 96, 100, 108])
+
+        cls.hai34_string_not_exists = Hai34String('7z')
+        cls.hai34_counter_not_exists = cls.hai34_string_not_exists.to_hai34_counter()
+        cls.hai34_list_not_exists = cls.hai34_string_not_exists.to_hai34_list()
+        cls.hai136_list_not_exists = Hai136List([135])
+
+        cls.hai34_string_diff = Hai34String('1m9s1z')
+        cls.hai34_counter_diff = cls.hai34_string_diff.to_hai34_counter()
+        cls.hai34_list_diff = cls.hai34_string_diff.to_hai34_list()
+        cls.hai136_list_diff = Hai136List([0, 104, 109])
+
+    def test_sub_hai34_counter_and_hai34_counter(self):
+        result = self.hai34_counter1 - self.hai34_counter2
+        expected = self.hai34_counter_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai34_counter_and_hai34_list(self):
+        result = self.hai34_counter1 - self.hai34_list2
+        expected = self.hai34_counter_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai34_counter_and_hai34_string(self):
+        result = self.hai34_counter1 - self.hai34_string2
+        expected = self.hai34_counter_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai34_counter_and_hai136_list(self):
+        result = self.hai34_counter1 - self.hai136_list2
+        expected = self.hai34_counter_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai34_counter_and_hai34_counter_not_exists(self):
+        with self.assertRaises(ValueError):
+            self.hai34_counter1 - self.hai34_counter_not_exists
+
+    def test_sub_hai34_counter_and_int(self):
+        with self.assertRaises(TypeError):
+            self.hai34_counter1 - 1
+
+    def test_sub_hai34_list_and_hai34_counter(self):
+        result = self.hai34_list1 - self.hai34_counter2
+        expected = self.hai34_list_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai34_list_and_hai34_list(self):
+        result = self.hai34_list1 - self.hai34_list2
+        expected = self.hai34_list_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai34_list_and_hai34_string(self):
+        result = self.hai34_list1 - self.hai34_string2
+        expected = self.hai34_list_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai34_list_and_hai136_list(self):
+        result = self.hai34_list1 - self.hai136_list2
+        expected = self.hai34_list_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai34_list_and_hai34_list_not_exists(self):
+        with self.assertRaises(ValueError):
+            self.hai34_list1 - self.hai34_list_not_exists
+
+    def test_sub_hai34_list_and_int(self):
+        with self.assertRaises(TypeError):
+            self.hai34_list1 - 1
+
+    def test_sub_hai34_string_and_hai34_counter(self):
+        result = self.hai34_string1 - self.hai34_counter2
+        expected = self.hai34_string_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai34_string_and_hai34_list(self):
+        result = self.hai34_string1 - self.hai34_list2
+        expected = self.hai34_string_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai34_string_and_hai34_string(self):
+        result = self.hai34_string1 - self.hai34_string2
+        expected = self.hai34_string_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai34_string_and_hai136_list(self):
+        result = self.hai34_string1 - self.hai136_list2
+        expected = self.hai34_string_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai34_string_and_hai34_string_not_exists(self):
+        with self.assertRaises(ValueError):
+            self.hai34_string1 - self.hai34_string_not_exists
+
+    def test_sub_hai34_string_and_int(self):
+        with self.assertRaises(TypeError):
+            self.hai34_string1 - 1
+
+    def test_sub_hai136_list_and_hai34_counter(self):
+        with self.assertRaises(TypeError):
+            self.hai136_list1 - self.hai34_counter2
+
+    def test_sub_hai136_list_and_hai34_list(self):
+        with self.assertRaises(TypeError):
+            self.hai136_list1 - self.hai34_list2
+
+    def test_sub_hai136_list_and_hai34_string(self):
+        with self.assertRaises(TypeError):
+            self.hai136_list1 - self.hai34_string2
+
+    def test_sub_hai136_list_and_hai136_list(self):
+        result = self.hai136_list1 - self.hai136_list2
+        expected = self.hai136_list_diff
+        self.assertEqual(result.data, expected.data)
+
+    def test_sub_hai136_list_and_hai136_list_not_exists(self):
+        with self.assertRaises(ValueError):
+            self.hai136_list1 - self.hai136_list_not_exists
+
+    def test_sub_hai136_list_and_int(self):
+        hai136_list = self.hai136_list1
+        with self.assertRaises(TypeError):
+            hai136_list + 1
+
+
+if __name__ == '__main__':
+    unittest.main()
