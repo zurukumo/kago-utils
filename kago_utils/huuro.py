4b97a0c990d9893f0d055d3ed2a4cb09a66fb2cb
zurukumo <zurukumo@gmail.com>
2024-10-18 18:39:13 +0900

    fix: simplify Huuro classes

--- kago_utils/huuro.py
+++ kago_utils/huuro.py
@@ -25,17 +25,15 @@ class Chii:
         self.__validate_same_suit()
 
     def __validate_length_3(self) -> None:
-        if len(self.hais.to_list()) != 3:
+        if len(self.hais) != 3:
             raise ValueError('Invalid Chii: length should be 3')
 
     def __validate_consecutive(self) -> None:
-        hai_list = self.hais.to_list()
-        if not (hai_list[0] == hai_list[1] - 1 == hai_list[2] - 2):
+        if not (self.hais[0].number == self.hais[1].number - 1 == self.hais[2].number - 2):
             raise ValueError('Invalid Chii: should be consecutive')
 
     def __validate_same_suit(self) -> None:
-        hai_list = self.hais.to_list()
-        if not (hai_list[0] // 9 == hai_list[1] // 9 == hai_list[2] // 9):
+        if not (self.hais[0].suit == self.hais[1].suit == self.hais[2].suit):
             raise ValueError('Invalid Chii: should be the same suit')
 
 
@@ -46,22 +44,21 @@ class Pon:
 
     __slots__ = ('hais', 'stolen', 'from_who')
 
-    def __init__(self, hai: Hai136Group, stolen: Hai136, from_who: Zaichi) -> None:
-        self.hais = hai
+    def __init__(self, hais: Hai136Group, stolen: Hai136, from_who: Zaichi) -> None:
+        self.hais = hais
         self.stolen = stolen
         self.from_who = from_who
 
     def validate(self) -> None:
         self.validate_length_3()
-        self.validate_same_hai()
+        self.validate_same_face()
 
     def validate_length_3(self) -> None:
-        if len(self.hais.to_list()) != 3:
+        if len(self.hais) != 3:
             raise ValueError('Invalid Pon: length should be 3')
 
-    def validate_same_hai(self) -> None:
-        hai_list = self.hais.to_list()
-        if not (hai_list[0] == hai_list[1] == hai_list[2]):
+    def validate_same_face(self) -> None:
+        if not (self.hais[0].face == self.hais[1].face == self.hais[2].face):
             raise ValueError('Invalid Pon: should be the same hai')
 
 
@@ -83,15 +80,14 @@ class Kakan:
 
     def __validate(self) -> None:
         self.__validate_length_4()
-        self.__validate_same_hai()
+        self.__validate_same_face()
 
     def __validate_length_4(self) -> None:
-        if len(self.hais.to_list()) != 4:
+        if len(self.hais) != 4:
             raise ValueError('Invalid Kakan: length should be 4')
 
-    def __validate_same_hai(self) -> None:
-        hai_list = self.hais.to_list()
-        if not (hai_list[0] == hai_list[1] == hai_list[2] == hai_list[3]):
+    def __validate_same_face(self) -> None:
+        if not (self.hais[0].face == self.hais[1].face == self.hais[2].face == self.hais[3].face):
             raise ValueError('Invalid Kakan: should be the same hai')
 
 
@@ -109,37 +105,37 @@ class Daiminkan:
 
     def __validate(self) -> None:
         self.__validate_length_4()
-        self.__validate_same_hai()
+        self.__validate_same_face()
 
     def __validate_length_4(self) -> None:
-        if len(self.hais.to_list()) != 4:
+        if len(self.hais) != 4:
             raise ValueError('Invalid Daiminkan: length should be 4')
 
-    def __validate_same_hai(self) -> None:
-        hai_list = self.hais.to_list()
-        if not (hai_list[0] == hai_list[1] == hai_list[2] == hai_list[3]):
+    def __validate_same_face(self) -> None:
+        if not (self.hais[0].face == self.hais[1].face == self.hais[2].face == self.hais[3].face):
             raise ValueError('Invalid Daiminkan: should be the same hai')
 
 
 class Ankan:
     hais: Hai136Group
+    from_who: Zaichi
 
-    __slots__ = ('hais',)
+    __slots__ = ('hais', 'from_who')
 
     def __init__(self, hais: Hai136Group) -> None:
         self.hais = hais
+        self.from_who = Zaichi.JICHA
 
         self.__validate()
 
     def __validate(self) -> None:
         self.__validate_length_4()
-        self.__validate_same_hai()
+        self.__validate_same_face()
 
     def __validate_length_4(self) -> None:
         if len(self.hais.to_list()) != 4:
             raise ValueError('Invalid Ankan: length should be 4')
 
-    def __validate_same_hai(self) -> None:
-        hai_list = self.hais.to_list()
-        if not (hai_list[0] == hai_list[1] == hai_list[2] == hai_list[3]):
+    def __validate_same_face(self) -> None:
+        if not (self.hais[0].face == self.hais[1].face == self.hais[2].face == self.hais[3].face):
             raise ValueError('Invalid Ankan: should be the same hai')
