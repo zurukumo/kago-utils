eb968b2041e7f47db219a831979c4849e69b38d1
zurukumo <zurukumo@gmail.com>
2024-11-03 19:09:05 +0900

    fix: Remove `Hai34`

--- kago_utils/hai.py
+++ kago_utils/hai.py
@@ -3,79 +3,6 @@ from __future__ import annotations
 from typing import Literal
 
 
-class Hai34:
-    id: int
-
-    __slots__ = ('id',)
-
-    def __init__(self, id: int) -> None:
-        self.validate(id)
-        self.id = id
-
-    def validate(self, id: int) -> None:
-        if not isinstance(id, int):
-            raise TypeError(f"Invalid Hai34: id should be an integer, but got {type(id).__name__}")
-        if not 0 <= id <= 33:
-            raise ValueError(f"Invalid Hai34: id should be between 0 and 33, but got {id}")
-
-    def to_hai34(self) -> Hai34:
-        if isinstance(self, Hai34):
-            return self
-
-        raise TypeError(f"Unsupported operand type(s) for to_hai34: '{type(self).__name__}'")
-
-    @property
-    def suit(self) -> Literal['m', 'p', 's', 'z']:
-        if 0 <= self.id < 9:
-            return 'm'
-        elif 9 <= self.id < 18:
-            return 'p'
-        elif 18 <= self.id < 27:
-            return 's'
-        elif 27 <= self.id < 34:
-            return 'z'
-
-        raise ValueError(f"Invalid Hai34: id should be between 0 and 33, but got {self.id}")
-
-    @property
-    def number(self) -> int:
-        return self.id % 9 + 1
-
-    @property
-    def face(self) -> str:
-        return f"{self.number}{self.suit}"
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, Hai34):
-            return self.id == other.id
-
-        return False
-
-    def __lt__(self, other: object) -> bool:
-        if isinstance(other, Hai34):
-            return self.id < other.id
-
-        raise TypeError(f"Unsupported operand type(s) for <: '{type(self).__name__}' and '{type(other).__name__}'")
-
-    def __le__(self, other: object) -> bool:
-        if isinstance(other, Hai34):
-            return self.id <= other.id
-
-        raise TypeError(f"Unsupported operand type(s) for <=: '{type(self).__name__}' and '{type(other).__name__}'")
-
-    def __gt__(self, other: object) -> bool:
-        if isinstance(other, Hai34):
-            return self.id > other.id
-
-        raise TypeError(f"Unsupported operand type(s) for >: '{type(self).__name__}' and '{type(other).__name__}'")
-
-    def __ge__(self, other: object) -> bool:
-        if isinstance(other, Hai34):
-            return self.id >= other.id
-
-        raise TypeError(f"Unsupported operand type(s) for >=: '{type(self).__name__}' and '{type(other).__name__}'")
-
-
 class Hai136:
     id: int
 
@@ -90,12 +17,6 @@ class Hai136:
             raise TypeError(f"Invalid Hai136: id should be an integer, but got {type(id).__name__}")
         if not 0 <= id <= 135:
             raise ValueError(f"Invalid Hai136: id should be between 0 and 135, but got {id}")
-
-    def to_hai34(self) -> Hai34:
-        if isinstance(self, Hai136):
-            return Hai34(self.id // 4)
-
-        raise TypeError(f"Unsupported operand type(s) for to_hai34: '{type(self).__name__}'")
 
     @property
     def suit(self) -> Literal['m', 'p', 's', 'z']:
--- tests/test_hai.py
+++ tests/test_hai.py
@@ -1,24 +1,6 @@
 import unittest
 
-from kago_utils.hai import Hai34, Hai136
-
-
-class TestHai34Init(unittest.TestCase):
-    def test_init_with_valid_id(self):
-        for id in range(34):
-            Hai34(id)
-
-    def test_init_with_negative_id(self):
-        with self.assertRaises(ValueError):
-            Hai34(-1)
-
-    def test_init_with_too_large_id(self):
-        with self.assertRaises(ValueError):
-            Hai34(34)
-
-    def test_init_with_float_id(self):
-        with self.assertRaises(TypeError):
-            Hai34(0.0)
+from kago_utils.hai import Hai136
 
 
 class TestHai136Init(unittest.TestCase):
@@ -39,30 +21,6 @@ class TestHai136Init(unittest.TestCase):
             Hai136(0.0)
 
 
-class TestHai34ToHai34(unittest.TestCase):
-    def test_to_hai34(self):
-        for id in range(34):
-            self.assertEqual(Hai34(id).to_hai34(), Hai34(id))
-
-
-class TestHai136ToHai34(unittest.TestCase):
-    def test_to_hai34(self):
-        for id in range(136):
-            self.assertEqual(Hai136(id).to_hai34(), Hai34(id//4))
-
-
-class TestHai34Suit(unittest.TestCase):
-    def test_suit(self):
-        for id in range(9):
-            self.assertEqual(Hai34(id).suit, 'm')
-        for id in range(9, 18):
-            self.assertEqual(Hai34(id).suit, 'p')
-        for id in range(18, 27):
-            self.assertEqual(Hai34(id).suit, 's')
-        for id in range(27, 34):
-            self.assertEqual(Hai34(id).suit, 'z')
-
-
 class TestHai136Suit(unittest.TestCase):
     def test_suit(self):
         for id in range(36):
@@ -75,28 +33,10 @@ class TestHai136Suit(unittest.TestCase):
             self.assertEqual(Hai136(id).suit, 'z')
 
 
-class TestHai34Number(unittest.TestCase):
-    def test_number(self):
-        for id in range(34):
-            self.assertEqual(Hai34(id).number, id % 9 + 1)
-
-
 class TestHai136Number(unittest.TestCase):
     def test_number(self):
         for id in range(136):
             self.assertEqual(Hai136(id).number, (id // 4) % 9 + 1)
-
-
-class TestHai34Face(unittest.TestCase):
-    def test_face(self):
-        faces = [
-            '1m', '2m', '3m', '4m', '5m', '6m', '7m', '8m', '9m',
-            '1p', '2p', '3p', '4p', '5p', '6p', '7p', '8p', '9p',
-            '1s', '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s',
-            '1z', '2z', '3z', '4z', '5z', '6z', '7z'
-        ]
-        for id in range(34):
-            self.assertEqual(Hai34(id).face, faces[id])
 
 
 class TestHai136Face(unittest.TestCase):
@@ -119,21 +59,6 @@ class TestHai136Color(unittest.TestCase):
         self.assertEqual(Hai136(52).color, 'aka')
         self.assertEqual(Hai136(87).color, 'kuro')
         self.assertEqual(Hai136(88).color, 'aka')
-
-
-class TestHai34Comparison(unittest.TestCase):
-    def test_comparison(self):
-        self.assertEqual(Hai34(0), Hai34(0))
-        self.assertNotEqual(Hai34(0), Hai34(1))
-        self.assertLess(Hai34(0), Hai34(1))
-        self.assertLessEqual(Hai34(0), Hai34(0))
-        self.assertLessEqual(Hai34(0), Hai34(1))
-        self.assertGreater(Hai34(1), Hai34(0))
-        self.assertGreaterEqual(Hai34(0), Hai34(0))
-        self.assertGreaterEqual(Hai34(1), Hai34(0))
-
-    def test_eq_with_int(self):
-        self.assertNotEqual(Hai34(0), 0)
 
 
 class TestHai136Comparison(unittest.TestCase):
