61970f06bec27c5a93cd4d2a6d07b20627f7b72f
zurukumo <zurukumo@gmail.com>
2025-01-18 15:40:11 +0900

    fix: Add `TYPE_CHECKING` to avoid circular import

--- kago_utils/agari_calculator.py
+++ kago_utils/agari_calculator.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 
 import copy
+from typing import TYPE_CHECKING
 
 from kago_utils.actions import Ankan, Chii, Daiminkan, Kakan, Pon
-from kago_utils.game import Game
 from kago_utils.hai import Hai
 from kago_utils.hai_group import HaiGroup
 from kago_utils.player import Player
 from kago_utils.tehai_decomposer import TehaiBlock, TehaiDecomposer
+
+if TYPE_CHECKING:
+    from kago_utils.game import Game
 
 
 class AgariCalculator:
