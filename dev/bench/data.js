264b40891aad915f6220f6e34ad029c7c98ad199
github-action-benchmark <github@users.noreply.github.com>
2025-02-28 06:49:07 +0000

    add Benchmark (pytest) benchmark result for a421dd618d43e56c6b862ddd6590098c62c1da84

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1740725347130,
+  "repoUrl": "https://github.com/zurukumo/kago-utils",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "zurukumo@gmail.com",
+            "name": "zurukumo",
+            "username": "zurukumo"
+          },
+          "committer": {
+            "email": "zurukumo@gmail.com",
+            "name": "zurukumo",
+            "username": "zurukumo"
+          },
+          "distinct": true,
+          "id": "a421dd618d43e56c6b862ddd6590098c62c1da84",
+          "message": "feat: add benchmark",
+          "timestamp": "2025-02-28T15:46:15+09:00",
+          "tree_id": "f8538f5133e3c97f71faddc8c114526e93fcee08",
+          "url": "https://github.com/zurukumo/kago-utils/commit/a421dd618d43e56c6b862ddd6590098c62c1da84"
+        },
+        "date": 1740725346747,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_shanten_calculator.py::test_shanten_benchmark",
+            "value": 0.9304164558607975,
+            "unit": "iter/sec",
+            "range": "stddev: 0.003538415322826306",
+            "extra": "mean: 1.0747875252000199 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
