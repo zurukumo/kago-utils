fbc5e35f0813119e0a02dc53a0a0c385ae50cbd9
zurukumo <zurukumo@gmail.com>
2025-06-17 23:37:09 +0900

    fix: Remove distance table from sdist

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from setuptools import Extension, setup
+from setuptools import Extension, find_packages, setup
 
 setup(
+    name="kago_utils",
+    packages=find_packages(),
+    package_data={"kago_utils": ["py.typed"]},
+    include_package_data=True,
+    exclude_package_data={"kago_utils": ["resources/distance_tables/*.c"]},
     ext_modules=[
         Extension(
             name="kago_utils.ext._shanten",
             sources=["kago_utils/ext/_shanten.c"],
         ),
-    ]
+    ],
 )
